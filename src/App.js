7126287e75340435cddda01c238e5549ca3edee3
Daniel Nunez <dannunez97@gmail.com>
2023-04-08 02:30:31 -0500

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
-import MuiToolbar from '../src/components/homepage.js'
+import {MUIToolbar, MuiToolbar} from '../src/components/Sidebar.js'
 import MiniDrawer from "../src/components/Sidebar.js";
+import Home from "./pages/Home.js"
+import AboutMe from "./pages/AboutMe";
+import Progression from "./pages/Progression";
+import Resume from "./pages/Resume.js";
+import { Routes, Route, Link } from "react-router-dom";
+import Sidebar from "../src/components/Sidebar.js";
+import {Box, Toolbar, Typography} from "@mui/material";
 
 
 export default function App() {
     return (
-        <div>
-            <MiniDrawer/>
-        </div>
-
-
+        <Box sx={{display: 'flex'}}>
+            <Sidebar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<AboutMe/>} />
+                <Route path="/progression" element={<Progression />} />
+                <Route path="/resume" element={<Resume/>}/>
+            </Routes>
+        </Box>
     );
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -22,12 +22,12 @@ import MailIcon from '@mui/icons-material/Mail';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 import photo from '../assets/profilephoto.jpeg';
+import { Link } from "react-router-dom";
 
 
-function MUIToolbar() {
+
+export function MUIToolbar() {
     return (
-        <div>
-            <CssBaseline/>
             <AppBar
                 position="fixed"
                 sx={{width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px`}}
@@ -38,112 +38,52 @@ function MUIToolbar() {
                     </Typography>
                 </Toolbar>
             </AppBar>
-        </div>
     );
 }
 
 const drawerWidth = 240;
 const menuItems = [
     {text: "About Me", path: "/about", icon: (<InfoIcon/>)},
-    {text: "Work Experience", path: "/experience", icon: (<WorkIcon/>)},
+    {text: "Work Experience", path: "/resume", icon: (<WorkIcon/>)},
     {text: "Progression", path: "/progression", icon: (<ArticleIcon/>)},
-    {text: "Github", path: "/github", icon: (<GitHubIcon/>)},
 ];
 
 
-export default function PermanentDrawerLeft() {
+export default function Sidebar() {
 
     return (
-        <Box sx={{display: 'flex'}}>
-            <MUIToolbar/>
             <Drawer
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': {
-                        width: drawerWidth,
-                        boxSizing: 'border-box',
-                    },
-                }}
-                variant="permanent"
-                anchor="left"
-            >
-                <Box
-                    sx={{
-                        height: 120,
-                        width: 240,
-                    }}
-                    justifyContent="center"
-                    alignItems="center"
-                    display="flex"
-                >
-                    <Box
-                        component="img"
-                        sx={{
-                            height: 100,
-                            width: 100,
-                            borderRadius: '50%',
-                            ml: 'auto',
-                            mr: 'auto',
-
-                        }}
-                        src={photo}
-
-
-                    />
-
+                sx={{ width: drawerWidth, flexShrink: 0, '& .MuiDrawer-paper': { width: drawerWidth,
+                        boxSizing: 'border-box',},}} variant="permanent" anchor="left">
+                <Box sx={{ height: 120, width: 240,}} justifyContent="center" alignItems="center" display="flex">
+                    <Box component="img"
+                        sx={{ height: 100, width: 100, borderRadius: '50%', ml: 'auto', mr: 'auto', }} src={photo}/>
                 </Box>
                 <Box sx={{width: 240, ml: "auto", mr: "auto", textAlign: "center"}}>
                     <Typography>Daniel Nunez</Typography>
                     <Typography>Software Engineer</Typography>
-                    <Typography>Iowa '21</Typography>
+                    <Typography>5 YOE</Typography>
                 </Box>
 
                 <Divider/>
                 <List>
                     {menuItems.map((item) => (
                         <ListItem key={item.text} disablePadding>
-                            <ListItemButton>
+                            <ListItemButton component={Link} to={item.path}>
                                 <ListItemIcon>{item.icon}</ListItemIcon>
-                                <ListItemText primary={item.text}/>
+                                <ListItemText primary={item.text}></ListItemText>
                             </ListItemButton>
                         </ListItem>
                     ))}
+                    <ListItem key="Github" disablePadding>
+                        <ListItemButton href="https://www.github.com/dnunez20" target="_blank">
+                            <ListItemIcon><GitHubIcon/></ListItemIcon>
+                            <ListItemText primary="Github"></ListItemText>
+                        </ListItemButton>
+                    </ListItem>
                 </List>
             </Drawer>
-            <Box
-                component="main"
-                sx={{flexGrow: 1, bgcolor: 'background.default', p: 3}}
-            >
-                <Toolbar/>
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                    enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                    imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                    Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                    Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                    nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                    leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                    feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                    consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                    sapien faucibus et molestie ac.
-                </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                    eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                    neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                    tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                    sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                    tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                    gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                    et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                    tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                    eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                    posuere sollicitudin aliquam ultrices sagittis orci a.
-                </Typography>
-            </Box>
-        </Box>
+
+
     );
 }
